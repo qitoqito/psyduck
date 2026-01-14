196f07e8da9cccfa63a0d3dd72f499728e2adf77
可达鸭 <ii@iippcc.com>
2026-01-14 18:01:38 +0800

    5.2.8

--- config/profile.js
+++ config/profile.js
@@ -95,7 +95,7 @@ export default {
                 h5st: {
                     xcx: '4.9',
                     latest: '5.2',
-                    encrypt: '3ddd8c1ddc1dacdd5cddc859dd3ddc5cdde968dd3dcc5cdde978dd3decec5cdde9d8dd3ddcec5cddd8a9dd485cdddc1dacdd4814784e9dac2387a6282828ddfcdd5cddbca8d9dd485cddc8a989e9d809b8c8dd3d28bcec5cddfceca8d9dd485cddc8b919a9b878a9dd3ddd8c1ddc1dacdd5cdd9899dd',
+                    encrypt: 'dd3ddd7c1ddc1dacdd5cddc859dd3ddc5cdde968dd3dcc5cdde978dd3d6c5cdde9d8dd3ddcec5cddd8a9dd485cdddc1dacdd48663b8a78de70a062282828ddec2ddd5cddbca8d9dd485cddc8a989e9d809b8c8dd3d28bcec5cddfceca8d9dd485cddc8b919a9b878a9dd3ddd7c1ddc1dacdd5cdd9899',
                     extend: {},
                     storage: {}
                 }
