7063ae7aaece566d60aa5fe5c5fe474b00707722
可达鸭 <ii@iippcc.com>
2026-04-28 23:15:06 +0800

    5.3.3

--- config/profile.js
+++ config/profile.js
@@ -18,6 +18,12 @@ export default {
                         },
                         referer: "https://prodev.m.jd.com/",
                         shell: true,
+                        enhance: {
+                            iphone: 'platform=3&loginType=2&loginWQBiz=wegame&build=170275&screen=390*844&networkType=wifi&d_brand=iPhone&d_model=iPhone13,3&lang=zh_CN&osVersion=15.1.1&partner=-1&ext={"appType":"jdapp","systemType":"ios","bigScreen":false,"pageUrl":"https%3A%2F%2Fpro.m.jd.com%2Fmall%2Factive%2FB2Y13x641hwWfpsoRenCzfbz4jR%2Findex.html"}&cthr=1',
+                            android: 'platform=3&loginType=2&loginWQBiz=wegame&build=100987&screen=393*818&networkType=wifi&d_brand=Xiaomi&d_model=MI 8&lang=zh_CN&osVersion=10&partner=xiaomi001&&ext={"appType":"jdapp","systemType":"android","bigScreen":false,"pageUrl":"https%3A%2F%2Fpro.m.jd.com%2Fmall%2Factive%2FB2Y13x641hwWfpsoRenCzfbz4jR%2Findex.html"}&cthr=1',
+                            weixin: 'osVersion=AndroidOS&screen=400*833&d_brand=Xiaomi&d_model=Xiaomi&lang=zh-CN&networkType=&openudid=&aid=&oaid=&ext=%7B%22idfa%22%3A%22%22%7D',
+                            wechat: 'osVersion=IOS&screen=390*844&d_brand=iApple&d_model=iPhone&lang=zh-CN&networkType=&openudid=&aid=&oaid=&ext=%7B%22idfa%22%3A%22%22%7D'
+                        }
                         // request: true,
                         // token: true
                     },
@@ -95,7 +101,7 @@ export default {
                 h5st: {
                     xcx: '4.9',
                     latest: '5.3',
-                    encrypt: 'd6c5cdde9d8dd3ddcec5cddd8a9dd485cddcc1dacdd3d28dddc1d6c1dbcdd5cdd9899dd3ddddc1d6c1dbc78c978dd5cddc859dd485cdddc1d6c1dbc78c978dd485533fee42d4f497b282828cc5cddececa8d9dd3dbcec5cddfceca8d9dd485cddc8b919a9b878a9dd3ddddc1dcc1dacdd5cdd9899dd3ddddc1dcc1dacdd5cddc859dd3dcc5cdde968dd3d9c5cdde978dd3',
+                    encrypt: 'dd3d28ddfc1ddc1dacdd5cdd9899dd3dddfc1ddc1dac78c978dd5cddc859dd485cddfc1ddc1dac78c978dd3d28dddc1d6c1dbcdd5cdd9899dd3ddddc1d6c1dbc78c978dd5cddc859dd485cdddc1d6c1dbc78c978dd4877462780695b5062282828cc5cddececa8d9dd3dbcec5cddfceca8d9dd485cddc8b919a9b878a9dd3dddcc1dcc1dacdd5cdd9899dd3dddcc1dcc1dacdd5cddc859dd3dcc5cdde968dd3dbc5cdde978dd3decec5cdde9d8dd3dccec5cddd8a9dd485cddcc1dac',
                     extend: {},
                     storage: {}
                 }
